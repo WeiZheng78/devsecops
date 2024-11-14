e31b9a0e60181f151ff1e3af0f551353a76a3f19
WeiZheng78 <weizheng78@gmail.com>
2024-11-14 22:52:21 +0800

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #k8s-deployment.sh
-
-sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+sed -i "s#replace#weizheng78/numeric-app:${GIT_COMMIT}#g" k8s_deployment_service.yaml
+#sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 # kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # if [[ $? -ne 0 ]]; then
